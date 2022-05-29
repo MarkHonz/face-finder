e4f6ac26a88c633d5a92ae15e1990843868fedb8
markhonz <markhonz@protonmail.com>
2022-05-28 20:42:49 -0400

    Saved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Navigation />
+			<Logo />
+			<ImageLinkForm />
+			<FaceRecognition />
+		</div>
+	);
 }
 
 export default App;
